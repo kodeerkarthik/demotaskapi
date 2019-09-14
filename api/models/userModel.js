5b305143a6572c04b6c97277df039f4b68830b42
kodeerkarthik <kkrishnegowdaotb1@gmail.com>
2019-09-14 07:48:17 +0530

    commited

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -1,25 +1,21 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var UserSchema = new Schema({
-  firstname: {
+  name: {
     type: String,
-    required: 'Please Enter the firstname'
-  },
-  lastname: {
-    type: String,
-    required: 'Please Enter the lastname'
-  }, 
-  email: {
-    type: String,
-    required: 'Please Enter valid emailId'
+    // required: 'Please Enter the name'
   },
   password: {
     type: String,
-    required: 'Please Enter the current password'
+    // required: 'Please Enter the password'
   },
-  mobile: {
+  place: {
     type: String,
-    required: 'Please Enter mobile number'
+    // required: 'Please Enter the place'
+  },
+  DOB: {
+    type: String,
+    // required: 'Please Enter Date Of Birth'
   }, 
   Created_date: {
     type: String,
@@ -28,52 +24,3 @@ var UserSchema = new Schema({
 });
 module.exports = mongoose.model('UserInfo', UserSchema);
 
-var detailSchema =new Schema({
-  doctors:{ type:String, default:'25'},
-  patients:{ type:String, default:'77'},
-  wards:{ type:String, default:'80'},
-  staff:{ type:String, default:'88'}
-})
-module.exports = mongoose.model('details', detailSchema);
-
-var countSchema =new Schema({
-  days:{ type:String, default:'365'},
-  patients:{ type:String, default:'564'},
-  doctors:{type:String, default:'164'},
-  lab:{ type:String, default:'765'}
-})
-module.exports = mongoose.model('timercount', countSchema);
-
-var appointmentSchema =new Schema({
-  name: {
-    type:String
-  },
-  email: {
-    type:String
-  },
-  date:{
-    type:Date,
-    
-  },
-  message:{
-    type:String
-  }
-})
-module.exports = mongoose.model('appointment',appointmentSchema)
-
-var selectdoctorSchema =new Schema({
-  department: {
-    type:String
-  },
-  doctor: {
-    type:String
-  },
-  date:{
-    type:Date,
-    
-  },
-  patient:{
-    type:String
-  }
-})
-module.exports = mongoose.model('selectdoctor',selectdoctorSchema)
--- server.js
+++ server.js
@@ -12,11 +12,11 @@ app.use(function(req, res, next) {
   next();
 });
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/Users');
+mongoose.connect('mongodb://localhost/demodb');
 UserData = require('./api/models/userModel'); 
 var routes = require('./api/routes/UserRoutes'); 
 routes(app);
-app.set('port', (process.env.PORT || 8000));
+app.set('port', (process.env.PORT || 9000));
 app.use(cors());
 app.listen(app.get('port'), function(){
   console.log('Server started on port ' + app.get('port'));
