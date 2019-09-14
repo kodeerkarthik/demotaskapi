6922ade51585ec8a8a1e9b270de09b020d98fddc
kodeerkarthik <kkrishnegowdaotb1@gmail.com>
2019-09-14 09:44:46 +0530

    completed

--- api/controller/UserController.js
+++ api/controller/UserController.js
@@ -16,63 +16,46 @@ exports.getAllUsers = function(req, res) {
   });
 };
 
-exports.userSignup = function(req, res){
+exports.userSignup = function(req, res) {
   console.log(req.body)
-    UserData.find({name: req.body.name},function(err, data){
-      if(data != null && data != ''){
-        res.send('User already exists');
-      }
-      else
-      {
-        var userData = new UserData(req.body);
-        bcrypt.genSalt(10, function(err, salt){
-          bcrypt.hash(userData.password, salt, function(err, hash) {
-            userData.password = hash;
-            userData.save(function(err, data){
-              if(err)
-                res.send(err.message);
-              res.json(data);
-            })
-          })
-        })
-      }
-    });
+  UserData.find({name: req.body.name},function(err, data){
+    if(data != null && data != ''){
+      res.send('User already exists');
+    }
+    else {
+      var userData = new UserData(req.body);
+      userData.save(function(err, data){
+        if(err)
+          res.send(err.message);
+        res.json(data);
+      });
+    }
+  })
 };
 
-exports.userSignin = (req,res,next) =>{
-  const name = req.body.name;
-  const password = req.body.password;
-  let loadedUser;
-  UserData.findOne({name: name})
-  .then(user =>{
-    if(!user){
-      const error = new Error('A user with this email could not be found.');
-      error.statusCode = 401;
-      throw error;
-    }
-    loadedUser = user;
-    return bcrypt.compare(password,user.password);
-  })
-  .then(isEqual =>{
-    if(!isEqual){
-      const error = new Error('wrong password.');
-      error.statusCode = 401;
-      throw error;
-    }
-    const token = jwt.sign(
-    {
-      name: loadedUser.name,
-      userId:loadedUser._id.toString()
-    },'secret')
-    return res.status(200).json({token: token, userId: loadedUser._id.toString(), email: loadedUser.email})
-  })
-  .catch(err => {
-    if (!err.statusCode) {
-      err.statusCode = 500;
-    }
-    next(err);
-  }); 
-}
 
+exports.userSignin = function(req,res){
+  UserData.find({name: req.body.name}, function(err, data){
+    if(data != null && data != ''){
+      if(req.body.password == data[0].password){
+          // res.status(200).json(data);
+          res.send("Login Succesfully");
+        }else{
+          res.send("Password does not matched");
+        }
+    } else{
+      res.send("User does not exists");
+    }
+  });
+};
+
+exports.updateUser = function(req, res) {
+  UserData.findOneAndUpdate({_id: req.params.userId}, 
+    req.body, {new: true}, function(err, data) {
+      if (err)
+        res.send(err);
+      res.json(data);
+    });
+};
 
 
--- api/routes/UserRoutes.js
+++ api/routes/UserRoutes.js
@@ -1,29 +1,14 @@
 module.exports = function(app) {
 	var userData = require('../controller/UserController');
-	// var hospitaldet=require('../controller/UserController');
-	// var timer=require('../controller/UserController');
-	// var appoint=require('../controller/UserController');
-	// var selectdoctor = require('../controller/UserController');
-	// var isAuth = require('../Middleware/isAuth')
+
 	app.route('/signup')
 		.post(userData.userSignup)
 		.get(userData.getAllUsers)
-	// app.route('/signin',isAuth)
-	// 	.post(userData.userSignin) 
-	// 	.get(userData.getAllSignin) 
-	// app.route('/details')
-	// 	.get(hospitaldet.hospitalDetails)
-	// app.route('/count')
-	// 	.get(timer.counting)
-	// app.route('/appointment')	
-	// 	.post(appoint.getAppointment)
-	// 	.get(appoint.getAppointmentDetails)	
-	// app.route('/selectdoctor')	
-	// 	.post(selectdoctor.postSelectDoctor)
-	// 	.get(selectdoctor.getSelectDoctor)
-	// app.route('/selectdoctor/:id')
-	// 	.delete(selectdoctor.deleteDoctor)
-	// 	.put(selectdoctor.updateAppointment)
+	app.route('/signin')
+		.post(userData.userSignin) 
+	app.route('/update/:userId')
+		.put(userData.updateUser);
+	
 };
 
 
